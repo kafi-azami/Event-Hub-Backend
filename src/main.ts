5fda5f4716ba948ecb8c16145caa34ea0b76af62
Kafi Ajuna <kafi_ajuna_33rpl@student.smktelkom-mlg.sch.id>
2026-06-01 10:04:34 +0700

    fix cors for vercel frontend

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,7 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-    ],
+    origin: true,
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
